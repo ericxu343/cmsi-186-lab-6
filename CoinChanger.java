ab2e906ac5df71d5b4da50c028d8557f726092d8
Eric Xu <ericxu343@gmail.com>
2020-04-18 13:33:15 -0400

    committing files to Lab 6

--- CoinChanger.java
+++ CoinChanger.java
@@ -1,4 +1,5 @@
 import java.util.Set;
+import java.util.*;
 // TODO of course, you may wish to import more things...
 
 public abstract class CoinChanger {
@@ -13,9 +14,26 @@ public abstract class CoinChanger {
         //   "At least one denomination is required"
         //   "Denominations must all be positive"
         //   "Denominations must have a 1-unit coin"
+        if (amount < 1) {
+          throw new IllegalArgumentException("Amount must be at least 1");
+        }
+        if (denominations.size() < 1) {
+          throw new IllegalArgumentException("At least one denomination is required");
+        }
+        for (Integer denomination: denominations) {
+          if (denomination < 1) {
+            throw new IllegalArgumentException("Denominations must all be positive");
+          }
+        }
+        if (denominations.contains(1) == false){
+          throw new IllegalArgumentException("Denominations must have a 1-unit coin");
+        }
+
     }
 
     public static class TopDown extends CoinChanger {
+
+
         public int minCoins(int amount, Set<Integer> denominations) {
             checkArguments(amount, denominations);
 
@@ -23,17 +41,82 @@ public abstract class CoinChanger {
             // do this recursively, so write a separate, private, recursive,
             // "helper" method. This method here will call that recursive
             // method with the memo object and the initial amount.
-            return 0; // TODO change this line, of course
+            int[] count = new int[amount];
+
+
+            if (amount < 1) {
+              return 0;
+            }
+
+            ArrayList<Integer> sortedList = new ArrayList<Integer>();
+            for (Integer denomination: denominations){
+              sortedList.add(denomination);
+            }
+            // sort the Array list
+            Collections.sort(sortedList);
+
+            return PerformRecursion(count, amount, sortedList); // do the recursion with position at 0
+            // TODO change this line, of course
+
+
         }
-    }
+
+
+        private int PerformRecursion(int[] count, int amount, ArrayList<Integer> sortDenominations) {
+            // Recursion Method
+            if (amount == 0){
+              return 0;
+            }
+            if (amount < 0){
+              return -1;
+            }
+
+            if (count[amount -1] != 0){
+              return count[amount -1];
+            }
+            int min = Integer.MAX_VALUE;
+            for (Integer denomination: sortDenominations){
+              int res = PerformRecursion(count, amount - denomination, sortDenominations);
+              if (res >= 0 && res < min){
+                min = res + 1;
+              }
+            }
+            count[amount -1] = (min == Integer.MAX_VALUE) ? -1: min;
+            return count[amount -1];
+
+        }
+
+
+        }
+
 
     public static class BottomUp extends CoinChanger {
         public int minCoins(int amount, Set<Integer> denominations) {
             checkArguments(amount, denominations);
 
+            ArrayList<Integer> sortedList = new ArrayList<Integer>();
+            for (Integer denomination: denominations){
+              sortedList.add(denomination);
+            }
+            // sort the Array list
+            Collections.sort(sortedList);
+
             // TODO: Implement this method using the bottom-up approach with
             // a table.
-            return 0; // TODO change this line, of course
+            int max = amount + 1;
+            int[] set = new int[amount + 1];
+            Arrays.fill(set, max);
+            set[0] = 0;
+            for (int i = 1; i <= amount; i++) {
+              for (int j = 0; j < sortedList.size(); j++) {
+                if (sortedList.get(j) <= i) {
+                  set[i] = Math.min(set[i], set[i - sortedList.get(j)] + 1);
+                }
+              }
+            }
+            return set[amount] > amount ? -1 : set[amount]; // TODO change this line, of course
         }
+
+
     }
 }
